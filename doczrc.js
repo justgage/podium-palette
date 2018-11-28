b634bf3003de8107568bba2eb2c80cb4408e8b11
Gage Peterson <justgage@gmail.com>
2018-11-27 22:52:09 -0700

    Write some Values

--- doczrc.js
+++ doczrc.js
@@ -4,11 +4,21 @@ export default {
   title: 'Podium CSS',
   description: 'Documentation of Podium CSS',
   // theme: 'src/theme/main',
+  menu: [
+    'Introduction',
+    'Values',
+    'Components',
+    'Layout',
+    'Style',
+    'Example',
+    'Table of Styles'
+  ],
   src: './src',
   themeConfig: {
     colors: {
       primary: '#4c76e0'
-    }
+    },
+    showPlaygroundEditor: true
   },
   plugins: [
     css({
